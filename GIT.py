43aaccb5a19d10ac1314daa1accfa7e67dd5028d
Irina Zheleztsova <zheleztzova.ira@yandex.ru>
2019-07-14 21:37:45 +0300

    .

--- GIT.py
+++ GIT.py
@@ -1,55 +1,96 @@
 from github import Github
 import requests
+import csv
+import copy
+import pandas as pd
 
-def login():
-    ACCESS_USERNAME =input("Please enter your Github username: ")
-    ACCESS_PWD = input("Please enter your account password: ")
-    client = Github(ACCESS_USERNAME, ACCESS_PWD, per_page=100)
-    user = client.get_user(ACCESS_USERNAME)
-    if user.public_repos == 0:
-        print("You don't have public repositories.")
-    repo_list = [repo.name for repo in user.get_repos() if not repo.fork]
-    print(repo_list)
-    python = 0
-    cplusplus = 0
-    javascript = 0
-    ruby = 0
-    java = 0
-    c = 0
-    css = 0
-    html = 0
-    tsql = 0
-    for j in repo_list:
-        repo = user.get_repo(j)
-        lang = repo.language
-        if lang == "Python":
-            python = python + 1
+ACCESS_USERNAME =input("Please enter your Github username: ")
+ACCESS_PWD = input("Please enter your account password: ")
+client = Github(ACCESS_USERNAME, ACCESS_PWD, per_page=100)
+user = client.get_user(ACCESS_USERNAME)
+if user.public_repos == 0:
+    print("You don't have public repositories.")
+repo_list = [repo.name for repo in user.get_repos() if not repo.fork]
+print(repo_list)
+python = 0
+cplusplus = 0
+javascript = 0
+ruby = 0
+java = 0
+c = 0
+css = 0
+html = 0
+tsql = 0
+php = 0
+cs=0
+shell = 0
+sum = 0
+for j in repo_list:
+    repo = user.get_repo(j)
+    lang = repo.language
+    if lang == "Python":
+        python += 1
+        sum += 1
 
-        elif lang == "JavaScript":
-            javascript = javascript + 1
+    elif lang == "JavaScript":
+        javascript += 1
+        sum += 1
 
-        elif lang == "Ruby":
-            ruby = ruby + 1
+    elif lang == "Ruby":
+        ruby += 1
+        sum += 1
 
-        elif lang == "C++":
-            cplusplus = cplusplus + 1
+    elif lang == "C++":
+        cplusplus += 1
+        sum += 1
 
-        elif lang == "Java":
-            java = java + 1
+    elif lang == "Java":
+        java += 1
+        sum += 1
 
-        elif lang == "C":
-            c = c + 1
+    elif lang == "C":
+        c += 1
+        sum += 1
 
-        elif lang == "CSS":
-            css = css + 1
+    elif lang == "CSS":
+        css = css + 1
+        sum += 1
 
-        elif lang == "HTML":
-            html = html + 1
+    elif lang == "HTML":
+        html += 1
+        sum += 1
 
-        elif lang == "TSQL":
-            tsql = tsql + 1
-        print(j,':',lang)
-    res_login=[]
+    elif lang == "TSQL":
+        tsql += 1
+        sum += 1
+
+    elif lang == "C#":
+        cs += 1
+        sum += 1
+
+    elif lang == "PHP":
+        php += 1
+        sum += 1
+
+    elif lang == "Shell":
+        shell += 1
+        sum += 1
+    print(j,':',lang)
+res_login=[]
+if (sum > 0):
+    res_login.append(python / sum)
+    res_login.append(javascript / sum)
+    res_login.append(ruby / sum)
+    res_login.append(cplusplus / sum)
+    res_login.append(java / sum)
+    res_login.append(c / sum)
+    res_login.append(css / sum)
+    res_login.append(html / sum)
+    res_login.append(tsql / sum)
+    res_login.append(cs / sum)
+    res_login.append(php / sum)
+    res_login.append(shell / sum)
+else:
     res_login.append(python)
     res_login.append(javascript)
     res_login.append(ruby)
@@ -59,84 +100,41 @@ def login():
     res_login.append(css)
     res_login.append(html)
     res_login.append(tsql)
+    res_login.append(cs)
+    res_login.append(php)
+    res_login.append(shell)
+print(res_login)
 
-    print("Python:",python, "JS:",javascript, "Ruby:", ruby, "C++",cplusplus, "Java:",java,"C",c, "CSS:",css,"HTML",html,
-        "TSQL:",tsql,res_login)
+def prior(res):
+    res_log=copy.deepcopy(res)
+    copy_res=copy.deepcopy(res)
+    prior=[0]*len(copy_res)
+    print(prior,copy_res,res_log)
+    p=15
+    for i in range (0,len(copy_res)):
+        imax=copy_res.index(max(copy_res))
+        for j in range (0,len(copy_res)):
+            if ((res_log[imax])==(copy_res[j])):
+                prior[j]=p
+                copy_res[j]=-1
+        p-=1
+    print(prior)
+    interest(res,prior)
 
-    def get_users_set(self, count=50, since=20000000):
-        users = self.get_users(since=since)
-        users_result = []
-        for user, _ in zip(users, range(count)):
-            users_result.append(user.login)
-        return users_result
+def interest(res,prior):
+    interest_k=[0]*len(res)
+    sum=0
+    for i in range(0,len(res)):
+        interest_k[i]=res[i]*prior[i]
+        sum+=interest_k
+    print(interest_k)
 
-    get_users_d=get_users_set(client)
-    print(get_repositories(get_users_d))
-    print(get_users_d)
+filename="data4.csv"
+X=pd.read_csv(filename)
+#print(X)
+prior(res_login)
 
 
-def get_repositories(get_users_d):
-    result = []
-    for i in range(0,len(get_users_d)):
-        res=[]
-        python = 0
-        cplusplus = 0
-        javascript = 0
-        ruby = 0
-        java = 0
-        c = 0
-        css = 0
-        html = 0
-        tsql = 0
-        url = "https://api.github.com/users/{0}/repos".format(get_users_d[i])
-        print(url,len(get_users_d))
 
-        r = requests.get(url=url)
-        if 'next' in r.links:
-            result += get_repositories(r.links['next']['url'])
 
-        for repository in r.json():
-            lang=repository.get('language')
-            result.append(repository.get('name'))
-            result.append(repository.get('language'))
-            if lang == "Python":
-                python = python + 1
-
-            elif lang == "JavaScript":
-                javascript = javascript + 1
-
-            elif lang == "Ruby":
-                ruby = ruby + 1
-
-            elif lang == "C++":
-                cplusplus = cplusplus + 1
-
-            elif lang == "Java":
-                java = java + 1
-
-            elif lang == "C":
-                c = c + 1
-
-            elif lang == "CSS":
-                css = css + 1
-
-            elif lang == "HTML":
-                html = html + 1
-
-            elif lang == "TSQL":
-                tsql = tsql + 1
-        res.append(python)
-        res.append(javascript)
-        res.append(ruby)
-        res.append(cplusplus)
-        res.append(java)
-        res.append(c)
-        res.append(css)
-        res.append(html)
-        res.append(tsql)
-        result.append(res)
-
-    return result
-
-login()
 
---
+++ data.py
@@ -0,0 +1,138 @@
+from github import Github
+import requests
+import csv
+
+def login():
+    ACCESS_USERNAME =input("Please enter your Github username: ")
+    ACCESS_PWD = input("Please enter your account password: ")
+    client = Github(ACCESS_USERNAME, ACCESS_PWD, per_page=100)
+    user = client.get_user(ACCESS_USERNAME)
+    if user.public_repos == 0:
+        print("You don't have public repositories.")
+
+    def get_users_set(self, count=60, since=20000000):
+        users = self.get_users(since=since)
+        users_result = []
+        for user, _ in zip(users, range(count)):
+            users_result.append(user.login)
+        return users_result
+
+    get_users_d=get_users_set(client)
+    print(get_repositories(get_users_d))
+    print(get_users_d)
+
+def get_repositories(get_users_d):
+    result = []
+    headline=['url','python','js','ruby','c++','java','c','css','html','tsql','C#','php','shell']
+    for i in range(0,len(get_users_d)):
+        res=[]
+        python = 0
+        cplusplus = 0
+        javascript = 0
+        ruby = 0
+        java = 0
+        c = 0
+        css = 0
+        html = 0
+        tsql = 0
+        cs=0
+        php=0
+        shell=0
+        sum=0
+        url = "https://api.github.com/users/{0}/repos".format(get_users_d[i])
+        print(url,len(get_users_d))
+
+        r = requests.get(url=url)
+        if 'next' in r.links:
+            result += get_repositories(r.links['next']['url'])
+
+        for repository in r.json():
+            lang=repository.get('language')
+            if lang == "Python":
+                python+=1
+                sum+=1
+
+            elif lang == "JavaScript":
+                javascript +=1
+                sum += 1
+
+            elif lang == "Ruby":
+                ruby +=1
+                sum += 1
+
+            elif lang == "C++":
+                cplusplus +=1
+                sum += 1
+
+            elif lang == "Java":
+                java +=1
+                sum += 1
+
+            elif lang == "C":
+                c +=1
+                sum += 1
+
+            elif lang == "CSS":
+                css = css + 1
+                sum += 1
+
+            elif lang == "HTML":
+                html +=1
+                sum += 1
+
+            elif lang == "TSQL":
+                tsql +=1
+                sum += 1
+
+            elif lang == "C#":
+                cs +=1
+                sum += 1
+
+            elif lang == "PHP":
+                php +=1
+                sum += 1
+
+            elif lang == "Shell":
+                shell +=1
+                sum += 1
+        if (sum>0):
+            res.append(url)
+            res.append(python/sum)
+            res.append(javascript/sum)
+            res.append(ruby/sum)
+            res.append(cplusplus/sum)
+            res.append(java/sum)
+            res.append(c/sum)
+            res.append(css/sum)
+            res.append(html/sum)
+            res.append(tsql/sum)
+            res.append(cs/sum)
+            res.append(php/sum)
+            res.append(shell/sum)
+            result.append(res)
+        else:
+            res.append(url)
+            res.append(python)
+            res.append(javascript)
+            res.append(ruby)
+            res.append(cplusplus)
+            res.append(java)
+            res.append(c)
+            res.append(css)
+            res.append(html)
+            res.append(tsql)
+            res.append(cs)
+            res.append(php)
+            res.append(shell)
+            result.append(res)
+
+    print(len(result),result)
+    with open('data4.csv', 'w', newline='') as f:
+        w = csv.writer(f)
+        w.writerow([g for g in headline])
+        for i in range(0,len(result)):
+            w.writerow((result[i]))
+        f.close()
+    return result
+
+login()
