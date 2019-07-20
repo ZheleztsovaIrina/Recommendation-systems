11cfddd167e7a4b9bf23d9685a80c654a19b1195
Irina Zheleztsova <zheleztzova.ira@yandex.ru>
2019-07-20 22:37:19 +0300

    .

--- views/recommend.py
+++ views/recommend.py
@@ -11,8 +11,15 @@ def login():
     password= request.args.get('password')
     return(jsonify(get_neighbours_by_login(username,password)))
 
-
-
 def get_neighbours_by_login(username,password):
     return{1:login_git(username,password)[0],2:login_git(username,password)[1],3:login_git(username,password)[2],
            4:login_git(username,password)[3],5:login_git(username,password)[4]}
+
+@app.errorhandler(404)
+def not_found_error(error):
+    return {400:"Bad request"}
+
+
+@app.errorhandler(500)
+def internal_error(error):
+    return {500:"Wrong username or password"}
